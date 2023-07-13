514182d0b7bbaad8a7143dc0c269620aee23c84c
Professor Dirty <103500840+wubinwww@users.noreply.github.com>
2023-07-13 17:18:27 +0800

    Add files via upload

--- PKHeX.Core/Saves/Substructures/Gen7/PlayerBattleStyle7.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/PlayerBattleStyle7.cs
@@ -1,18 +1,18 @@
-﻿namespace PKHeX.Core;
+namespace PKHeX.Core;
 
 public enum PlayerBattleStyle7
 {
-    Normal,
-    Elegant,
-    Girlish,
-    Reverent,
-    Smug,
-    LeftHanded,
-    Passionate,
-    Idol,
+    一般风格,
+    华丽风格,
+    少女风格,
+    祈祷风格,
+    装腔风格,
+    左手风格,
+    热情风格,
+    偶像风格,
 
     /// <summary>
     /// USUM Only
     /// </summary>
-    Nihilist,
+    虚无风格,
 }
