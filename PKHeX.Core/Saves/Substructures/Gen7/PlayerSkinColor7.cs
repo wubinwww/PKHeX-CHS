9d3b19cc569bcb505c9687623b8dd323dde50992
Professor Dirty <103500840+wubinwww@users.noreply.github.com>
2023-07-13 17:22:30 +0800

    Add files via upload

--- PKHeX.Core/Saves/Substructures/Gen7/PlayerSkinColor7.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/PlayerSkinColor7.cs
@@ -1,13 +1,13 @@
-﻿namespace PKHeX.Core;
+namespace PKHeX.Core;
 
 public enum PlayerSkinColor7
 {
-    M_Pale,
-    F_Pale,
-    M_Default,
-    F_Default,
-    M_Tan,
-    F_Tan,
-    M_Dark,
-    F_Dark,
+    男白肤色,
+    女白肤色,
+    男默认肤色,
+    女默认肤色,
+    男小麦肤色,
+    女小麦肤色,
+    男黑肤色,
+    女黑肤色,
 }
