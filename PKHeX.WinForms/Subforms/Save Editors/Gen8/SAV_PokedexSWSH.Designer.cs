82b842535b9cc71fd94195bfa120ea8af8e13db3
Professor Dirty <103500840+wubinwww@users.noreply.github.com>
2023-07-16 21:45:15 +0800

    Add files via upload

--- PKHeX.WinForms/Subforms/Save Editors/Gen8/SAV_PokedexSWSH.Designer.cs
+++ PKHeX.WinForms/Subforms/Save Editors/Gen8/SAV_PokedexSWSH.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PKHeX.WinForms
+namespace PKHeX.WinForms
 {
     partial class SAV_PokedexSWSH
     {
@@ -310,42 +310,42 @@
             // 
             mnuSeenNone.Name = "mnuSeenNone";
             mnuSeenNone.Size = new System.Drawing.Size(201, 22);
-            mnuSeenNone.Text = "Seen none";
+            mnuSeenNone.Text = "从未看到";
             mnuSeenNone.Click += SeenNone;
             // 
             // mnuSeenAll
             // 
             mnuSeenAll.Name = "mnuSeenAll";
             mnuSeenAll.Size = new System.Drawing.Size(201, 22);
-            mnuSeenAll.Text = "Seen all";
+            mnuSeenAll.Text = "全部看到";
             mnuSeenAll.Click += SeenAll;
             // 
             // mnuCaughtNone
             // 
             mnuCaughtNone.Name = "mnuCaughtNone";
             mnuCaughtNone.Size = new System.Drawing.Size(201, 22);
-            mnuCaughtNone.Text = "Caught none";
+            mnuCaughtNone.Text = "从未抓到";
             mnuCaughtNone.Click += CaughtNone;
             // 
             // mnuCaughtAll
             // 
             mnuCaughtAll.Name = "mnuCaughtAll";
             mnuCaughtAll.Size = new System.Drawing.Size(201, 22);
-            mnuCaughtAll.Text = "Caught all";
+            mnuCaughtAll.Text = "全部捕获";
             mnuCaughtAll.Click += CaughtAll;
             // 
             // mnuComplete
             // 
             mnuComplete.Name = "mnuComplete";
             mnuComplete.Size = new System.Drawing.Size(201, 22);
-            mnuComplete.Text = "Complete Dex";
+            mnuComplete.Text = "完整的图鉴";
             mnuComplete.Click += CompleteDex;
             // 
             // mnuBattleCount
             // 
             mnuBattleCount.Name = "mnuBattleCount";
             mnuBattleCount.Size = new System.Drawing.Size(201, 22);
-            mnuBattleCount.Text = "Change All Battle Count";
+            mnuBattleCount.Text = "更改所有对战计数";
             mnuBattleCount.Click += ChangeAllCounts;
             // 
             // mnuFormNone
